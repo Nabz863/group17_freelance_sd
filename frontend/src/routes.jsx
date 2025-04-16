c96d566f766cc687b28ad94081c6a4f314678e9c
Nabz863 <2671907@students.wits.ac.za>
2025-04-16 21:40:06 +0200

    Removed NotFound reference for now from routes.jsx.

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -21,7 +21,7 @@ export default function RoutesComponent() {
     <Route path="/unauthorized" element={<Unauthorised />} />
     <Route path="/register-role" element={<RegisterRole />} />
     {/* fallback route */}
-    <Route path="*" element={<NotFound />} />
+    {/*<Route path="*" element={<NotFound />} />*/}
   </Routes>
   );
 }
