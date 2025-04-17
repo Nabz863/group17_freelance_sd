28abd1d52267651014d7c50fe3c3e420503c22df
Nabeel Mahomed <2671907@students.wits.ac.za>
2025-04-17 08:00:21 +0200

    Update Login.jsx
    Basic Login page, will reintegrate old UI later

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function Login() {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <button onClick={() => loginWithRedirect()}>Log In / Sign Up</button>
+    </div>
+  );
+}
