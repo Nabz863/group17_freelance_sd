38bb177d337a78e564845c6808d9c05a57c3d699
Nabz863 <2671907@students.wits.ac.za>
2025-04-18 16:40:05 +0200

    Fixed routes.jsx error

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,24 @@
+import PendingApproval from "./pages/PendingApproval";
+import AdminDashboard from "./pages/AdminDashboard";
+import ClientDashboard from "./pages/ClientDashboard";
+import FreelancerDashboard from "./pages/FreelancerDashboard";
+import Unauthorised from "./pages/Unauthorised";
+import RegisterRole from "./pages/RegisterRole";
+import Landing from "./pages/Landing";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+export default function RoutesComponent() {
+    return (
+    <Routes>
+    <Route path="/" element={<Landing />} />
+    <Route path="/pending" element={<PendingApproval />} />
+    <Route path="/admin" element={<AdminDashboard />} />
+    <Route path="/client" element={<ClientDashboard />} />
+    <Route path="/freelancer" element={<FreelancerDashboard />} />
+    <Route path="/unauthorized" element={<Unauthorised />} />
+    <Route path="/register-role" element={<RegisterRole />} />
+    <Route path="*" element={<div className="p-8 text-center text-white">404 - Not Found</div>} />
+  </Routes>
+  );
+}
