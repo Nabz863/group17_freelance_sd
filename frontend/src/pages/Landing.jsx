a23b7a284ec5c243f563704501a48b48eda0387e
Nabz863 <2671907@students.wits.ac.za>
2025-04-20 13:15:52 +0200

    Fixed import in Landing.jsx

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import "../styles/Landing.css";
+import "./styles/Landing.css";
 
 export default function Landing() {
   const { loginWithRedirect } = useAuth0();
