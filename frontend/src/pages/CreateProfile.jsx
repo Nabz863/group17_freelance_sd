409816eb50ffbc1a4d6a4b95369c510918a6f83a
Nabz863 <2671907@students.wits.ac.za>
2025-04-21 18:42:52 +0200

    Saved a file

--- frontend/src/pages/CreateProfile.jsx
+++ frontend/src/pages/CreateProfile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import supabase from "../utils/supabaseClient";
-import "./styles/theme.css";
+import "../styles/theme.css";
 
 export default function CreateProfile() {
   const { user } = useAuth0();
